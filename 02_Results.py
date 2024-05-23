56615d471151969f35a68be6e6bd801d507ea72d
ojcm1991 <30807918+ojcm1991@users.noreply.github.com>
2024-05-23 19:03:28 +0800

    Add files via upload
    update

---
+++ 02_Results.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Results")
+st.write("Here you can put each of your key results.")
+
+st.header("1. Top customer spending behavior by category")
+st.image("../images/total_spending.png")
+st.text("Travel is the top spenders'")
